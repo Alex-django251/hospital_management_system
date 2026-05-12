03feae0bcbaab8143ca0b336b6a4a19d5c5c9755
salam lakhan <119106199+salamlakhan7@users.noreply.github.com>
2026-05-12 12:25:29 +0500

    b

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# Create a virtual environment
-python3.12 -m venv venv
-source venv/bin/activate
-
-# Install dependencies
-pip install --upgrade pip
 pip install -r requirements.txt
-
-# Run Django commands
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3.12 manage.py collectstatic --no-input
+python3.12 manage.py migrate
