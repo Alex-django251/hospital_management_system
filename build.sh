0db8150be3ab93150213bbde32b89172d9a03538
salam lakhan <119106199+salamlakhan7@users.noreply.github.com>
2026-05-12 12:21:52 +0500

    command error run

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Create a virtual environment
+python3.12 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
